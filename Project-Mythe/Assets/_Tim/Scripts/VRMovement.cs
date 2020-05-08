8c3be3d7e0600942149287d1dd494d01d7b7a399
Tim Bouwman <tim210201.tb@gmail.com>
2020-05-08 21:13:57 +0200

    begonnen aan de player
    ik heb steam vr toegevoegt en heb een basic scene in elkaar gezet om de player te testen ook heb ik een klein begin gemaakt aan de player controller

---
+++ Project-Mythe/Assets/_Tim/Scripts/VRMovement.cs
@@ -0,0 +1,31 @@
+﻿//author: Tim Bouwman
+//Github: https://github.com/TimBouwman
+using UnityEngine;
+using Valve.VR;
+
+/// <summary>
+/// 
+/// </summary>
+[RequireComponent(typeof(CharacterController))]
+public class VRMovement : MonoBehaviour
+{
+    #region Variables
+    [Header("Value's")]
+    [SerializeField] private float speed;
+
+    [Header("Actions")]
+    [SerializeField] private SteamVR_Action_Vector2 moveInput;
+    #endregion
+
+    #region Unity Methods
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    #endregion
+}
