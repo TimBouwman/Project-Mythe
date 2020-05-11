89bd8b3ee9164da522e82549bb80057136cb64d2
jamirodevulder <31829657+jamirodevulder@users.noreply.github.com>
2020-05-11 21:54:33 +0200

    code nog netter

--- Project-Mythe/Assets/_Jamiro/US2_SoundHandler/Scripts/ItemSound.cs
+++ Project-Mythe/Assets/_Jamiro/US2_SoundHandler/Scripts/ItemSound.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class ItemSound : MonoBehaviour
 {
     #region Variables
+    //variable voor audio objecten die gepooled word
     [SerializeField] [Tooltip("geluidje wat het object moet maken als hij iets raakt")] private AudioClip objectHitSomethingSound;
-    [SerializeField] [Tooltip("geluidje wat het object moet maken wanneer je het object gebruikt")] private AudioClip objectSoundWhenUsed;
-    [SerializeField] [Tooltip("de audiosource die op dit item moet")] private AudioSource thisObjectAudioSource;
-    [SerializeField] [Tooltip("de magnitude van de velocity / 100  en dan nog eens delen door dit")]private float soundDevideBy;
+    [SerializeField] [Tooltip("de magnitude van de velocity / 100  en dan nog eens delen door dit")] private float soundDevideBy;
     private AudioSource objectPoolingAudioSource;
     private float mag;
+
+    // variable voor audio wat van dit object komt;
+    [SerializeField] [Tooltip("geluidje wat het object moet maken wanneer je het object gebruikt")] private AudioClip objectSoundWhenUsed;
+    [SerializeField] [Tooltip("de audiosource die op dit item moet")] private AudioSource thisObjectAudioSource;
+    [SerializeField] [Tooltip("3d of 2d geluid voor dit item. 1/0")] private float ThisItemIsUsedAudioDimension = 1;
+    [SerializeField] [Tooltip("minimale distance van het object om het geluid te horen")] private float ThisItemIsUsedAudioMinDistance = 0;
+    [SerializeField] [Tooltip("maximale distance van het object om het geluid te horen")] private float ThisItemIsUsedAudioMaxDistance = 5;
+
     #endregion
 
     #region Unity methods
     private void Start()
     {
-        if(thisObjectAudioSource != null) thisObjectAudioSource = GetComponent<AudioSource>();
+        if (thisObjectAudioSource != null) thisObjectAudioSource = GetComponent<AudioSource>();
     }
-    #endregion
-
-    #region Sound methods
-    // checkt voor collision met de grond
-    // set de enum zodat de juiste sound settings word gebruikt
+    // checkt voor collision met iets en als het hard genoeg is speeld ie geluid af
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.relativeVelocity.magnitude >= 3)
@@ -31,9 +34,23 @@ public class ItemSound : MonoBehaviour
             PoolItemSound();
         }
     }
+    #endregion
 
-    //vraagt de objectpooler een audiohandler object uit de pool aan 
-    //past de settings van het geluid aan waar dat nodig is (volume van het item wat de grond raakt word bepaald door hoe hard hij iets raakt)
+    #region Sound methods
+    
+    private void SetAudioSettingForThisObject()
+    {
+        thisObjectAudioSource.clip = objectSoundWhenUsed;
+        thisObjectAudioSource.spatialBlend = ThisItemIsUsedAudioDimension;
+        thisObjectAudioSource.minDistance = ThisItemIsUsedAudioMinDistance;
+        thisObjectAudioSource.maxDistance = ThisItemIsUsedAudioMaxDistance;
+        thisObjectAudioSource.playOnAwake = false;
+        thisObjectAudioSource.loop = false;
+    }
+    
+
+    //vraagt van de objectpooler een audio object uit de pool aan 
+    
     private void PoolItemSound()
     {
         GameObject sound = ObjectPooler.SharedInstance.GetPooledObject();
@@ -43,9 +60,10 @@ public class ItemSound : MonoBehaviour
             SetHitSomethingSoundSettings(sound);
         }
     }
+    
 
-    // de enum word gecheckt en de volume word aangepast wanneer nodig is.
-    // en de juiste audioclip word gebruikt
+
+    // hoe hard het geluid zal zijn word bepaald en de goeie audio clip word mee gegeven.
     private void SetHitSomethingSoundSettings(GameObject sound)
     {
             objectPoolingAudioSource.volume = (mag / 100) / soundDevideBy;
@@ -53,11 +71,11 @@ public class ItemSound : MonoBehaviour
             sound.transform.position = transform.position;
             sound.SetActive(true);
     }
+
     // wanneer je een item gebruikt deze aanroepen
     // speelt het geluid af wanneer een item gebruikt word.
     private void ItemIsUsedSound()
     {
-        thisObjectAudioSource.clip = objectSoundWhenUsed;
         thisObjectAudioSource.Play();
     }
     #endregion
