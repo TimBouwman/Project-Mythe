b5accfc038b4b4240f3ed88440c8a6ee036373ec
Tim Bouwman <tim210201.tb@gmail.com>
2020-05-13 08:35:39 +0200

    the hand now rotates when near a  environment collider

--- Project-Mythe/Assets/_Tim/Scripts/ActiveRagdollHand.cs
+++ Project-Mythe/Assets/_Tim/Scripts/ActiveRagdollHand.cs
@@ -1,6 +1,7 @@
 ﻿//author: Tim Bouwman
 //Github: https://github.com/TimBouwman
 using UnityEngine;
+using Valve.VR;
 
 /// <summary>
 /// 
@@ -9,15 +10,21 @@ using UnityEngine;
 public class ActiveRagdollHand : MonoBehaviour
 {
     #region Variables
-    [Tooltip("Overlap Sphere")]
+    [Header("OverlapSphere")]
     [SerializeField] private Vector3 center;
-    [SerializeField] private float radius = 0.1f;
-    [SerializeField] private LayerMask itemLayer;
+    [Space(7f)]
     [SerializeField] private LayerMask environmentLayer;
+    [SerializeField] private float environmentRadius = 0.2f;
+    [Space(7f)]
+    [SerializeField] private LayerMask itemLayer;
+    [SerializeField] private float itemRadius = 0.1f;
     private LayerMask layer;
-    Collider[] colliders;
-    [Tooltip("Objects")]
-    [SerializeField] private Transform HandLocation;
+
+    [Header("Objects")]
+    [SerializeField] private Transform controller;
+    [Tooltip("This is the object that will rotate to the nearest collider but it is not the object the rotation will be read from")]
+    [SerializeField] private Transform turnIndexHolder;
+    [Tooltip("This is the object the rotation for the hand is read from")]
     [SerializeField] private Transform turnIndex;
     private Rigidbody rb;
     #endregion
@@ -39,7 +46,10 @@ public class ActiveRagdollHand : MonoBehaviour
     }
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(this.transform.position + center, radius);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position + center, environmentRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position + center, itemRadius);
     }
     #endregion
 
@@ -49,29 +59,51 @@ public class ActiveRagdollHand : MonoBehaviour
     /// </summary>
     private void UpdateHandPos()
     {
-        this.transform.position = HandLocation.position;
+        this.transform.position = controller.position;
     }
     /// <summary>
     /// 
     /// </summary>
     private void UpdateHandRot()
     {
-        //this.transform.rotation = HandLocation.rotation;
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position + center, environmentRadius, environmentLayer);
+        if (colliders.Length > 0)
+        {
+            //Look for the closest point inside OverlapSphere
+            float minDistance = environmentRadius;
+            Collider closestCollider = colliders[0];
+            foreach (Collider collider in colliders)
+            {
+                //get the 2 points
+                Vector3 a = this.transform.position;
+                Vector3 b = collider.ClosestPoint(a);
+                //compare the distance between point a and point b
+                if ((a - b).sqrMagnitude < minDistance * minDistance)
+                {
+                    //if the distance is smaller than minDistance save that value as the new minDistance
+                    //and same the collider as closestCollider
+                    minDistance = Vector3.Distance(a, b);
+                    closestCollider = collider;
+                }
+            }
+            //turn object to the closest collider for a reference rotation 
+            turnIndexHolder.LookAt(closestCollider.ClosestPoint(this.transform.position));
+            //get current distance between the hand and the closest collider
+            float currentDistance = Vector3.Distance(closestCollider.ClosestPoint(this.transform.position), this.transform.position);
+            //transition from the controller rotation to the turn idex rotation depending on
+            //the current distance between the hand and the collider
+            float i = currentDistance / environmentRadius;
+            this.transform.rotation = Quaternion.Lerp(turnIndex.transform.rotation, controller.rotation, i);
+            turnIndexHolder.LookAt(closestCollider.ClosestPoint(this.transform.position));
+        }
+        else this.transform.rotation = controller.rotation;
     }
     /// <summary>
     /// 
     /// </summary>
     private void GrabHandler()
     {
-        colliders = Physics.OverlapSphere(this.transform.position + center, radius, layer);
-        foreach (Collider collider in colliders)
-        {
-            turnIndex.LookAt(collider.ClosestPoint(this.transform.position));
-            float x = Vector3.Distance(collider.ClosestPoint(this.transform.position), this.transform.position);
-            Debug.Log(x);
-            float t = x / radius;
-            this.transform.rotation = Quaternion.Lerp(collider.transform.rotation, HandLocation.rotation, t);
-        }
+        
     }
     #endregion
 }
