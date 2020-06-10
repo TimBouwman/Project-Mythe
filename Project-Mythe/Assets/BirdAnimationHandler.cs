4324d76bf9975bb697d391fd70a27e95aeb03acd
jamirodevulder <31829657+jamirodevulder@users.noreply.github.com>
2020-06-10 11:39:07 +0200

    animatie

---
+++ Project-Mythe/Assets/BirdAnimationHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Valve.VR.InteractionSystem;
+
+public class BirdAnimationHandler : MonoBehaviour
+{
+    [SerializeField] private Animator thisAnimtor;
+    [SerializeField] private BirdHandler birdHandler;
+    [SerializeField] private Transform playerTransform;
+    private string animKillBool = "killwalk";
+    private string walkAnimBool = "normalwalk";
+    private float timeleft = 1.9f;
+    private bool timer = false;
+    void Start()
+    {
+        if(thisAnimtor == null)
+        {
+            thisAnimtor = GetComponent<Animator>();
+        }
+
+
+    }
+    void Update()
+    {
+        if(timer)
+        {
+            if (timeleft > 0)
+            {
+                timeleft -= Time.deltaTime;
+                print(timeleft);
+            }
+            else
+            {
+                timer = false;
+                birdHandler.GetAgent().destination = playerTransform.position;
+            }
+        }
+    }
+    public void SetAttackAnimation()
+    {
+        thisAnimtor.SetBool(animKillBool, true);
+     //   timeleft = thisAnimtor.GetCurrentAnimatorClipInfo(0)[0].clip.length;
+        timer = true;
+
+    }
+
+    public void SetWalkAnimation(bool goingToWalk)
+    {
+        thisAnimtor.SetBool(walkAnimBool, goingToWalk);
+    }
+
+   
+
+
+}
