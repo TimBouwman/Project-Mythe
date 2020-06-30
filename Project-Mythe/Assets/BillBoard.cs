e41b686634adf76fd851b9f634679148bf206f20
Tim Bouwman <tim210201.tb@gmail.com>
2020-06-30 08:51:40 +0200

    done

---
+++ Project-Mythe/Assets/BillBoard.cs
@@ -0,0 +1,25 @@
+﻿//author: Tim Bouwman
+//Github: https://github.com/TimBouwman
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class BillBoard : MonoBehaviour
+{
+    #region Variables
+    private Transform camera;
+    #endregion
+
+    #region Unity Methods
+    private void Start()
+    {
+        this.camera = GameObject.FindWithTag("PlayerHead").transform;
+    }
+
+    private void Update()
+    {
+        this.transform.LookAt(camera);
+    }
+    #endregion
+}
