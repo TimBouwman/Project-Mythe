3a81bd1cc9ba65f527a5b45098651d127edbca57
jamirodevulder <31829657+jamirodevulder@users.noreply.github.com>
2020-07-01 10:03:38 +0200

    latets thingies

---
+++ Project-Mythe/Assets/RestartGameTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartGameTrigger : MonoBehaviour
+{
+    private bool nextEvent = false;
+    [SerializeField] private GameObject deathBox = null;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!nextEvent && other.tag == "Player")
+        {
+            print("test");
+            deathBox.SetActive(true);
+            nextEvent = true;
+        }
+    }
+}
--- Project-Mythe/Assets/_Jamiro/US3_EnemyAI/Scripts/restartGame.cs
+++ Project-Mythe/Assets/_Jamiro/US3_EnemyAI/Scripts/restartGame.cs
@@ -4,6 +4,7 @@ using Valve.VR;
 public class restartGame : MonoBehaviour
 {
     [SerializeField] private SteamVR_Action_Boolean calibrate;
+
     void Update()
     {
         if (calibrate.GetState(SteamVR_Input_Sources.LeftHand) && calibrate.GetStateDown(SteamVR_Input_Sources.RightHand))
